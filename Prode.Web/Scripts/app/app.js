704ad2488dd4b88ba00df6a607a16a3c594528ca
Lucas Contreras <cont.lucas@gmail.com>
2017-06-21 21:55:40 -0300

    added a comment in app

--- Prode.Web/Scripts/app/app.js
+++ Prode.Web/Scripts/app/app.js
@@ -1,8 +1,8 @@
-﻿require(["angular"], function() {
+﻿require(["angular"], function () {
     var app = angular.module("app", []);
 
-    app.controller("AppController", function($scope) {
+    app.controller("AppController", function ($scope) {
         $scope.nombre = "lucas";
-
+        //se hizo un cambio en este archivo
     });
 });
