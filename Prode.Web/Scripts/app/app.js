aebbe255f8da752bad38d0ad9f44c8a1f67281c6
Lucas Contreras <cont.lucas@gmail.com>
2017-07-04 20:09:01 -0300

    Se estabilizo el branch

--- Prode.Web/Scripts/app/app.js
+++
@@ -1,8 +0,0 @@
-﻿require(["angular"], function () {
-    var app = angular.module("app", []);
-
-    app.controller("AppController", function ($scope) {
-        $scope.nombre = "lucas";
-        //se hizo un cambio en este archivo
-    });
-});
