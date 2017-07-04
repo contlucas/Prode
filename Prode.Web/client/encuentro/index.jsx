2d666934293b818f0090ccf212572c284875af5a
Hernán <silva.c.hernan@gmail.com>
2017-07-03 23:24:53 -0300

    Merge definitivo con Lucas

---
+++ Prode.Web/client/encuentro/index.jsx
@@ -0,0 +1,20 @@
+﻿var React = require("react");
+var ReactDOM = require("react-dom");
+
+class Encuentro extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { nombre: "hernan" };
+    }
+
+    render() {
+        return (
+            <div>
+                hola mundo prode! {this.state.nombre}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Encuentro />, document.getElementById("app"));
---
+++ Prode.Web/client/equipo/stores/equipoStore.js
@@ -0,0 +1,25 @@
+﻿import dispatcher from "../../dispatcher";
+import constantes from "../constants/equipoConstants";
+import { EventEmitter } from "events";
+
+class EquipoStoreClass extends EventEmitter {
+    saveEquipo(nombre) {
+        console.log("equipo " + nombre + " ha sido guardado correctamente");
+    }
+
+    handleChanges(action) {
+        switch (action.type) {
+            case constantes.SAVE_EQUIPO: {
+                this.saveEquipo(action.nombre);
+                break;
+            }
+            default: {
+                console.log("sin implementar", action.type);
+            }
+        }
+    }
+}
+
+const equipoStore = new EquipoStoreClass();
+dispatcher.register(equipoStore.handleChanges.bind(equipoStore));
+export default equipoStore;
