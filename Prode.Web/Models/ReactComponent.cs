05e7639d70a7cbdf352e8b5a1621242f1b9ca766
Lucas Contreras <cont.lucas@gmail.com>
2017-07-04 19:59:31 -0300

    se restauro el branch de lucas-developer

---
+++ Prode.Web/Models/ReactComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prode.Web.Models {
+    public class ReactComponent {
+        public string BundleName { get; set; }
+        public string BundleTitle { get; set; }
+    }
+}
---
+++ Prode.Web/client/equipo/actions/equipoActions.js
@@ -0,0 +1,11 @@
+﻿import dispatcher from "../../dispatcher";
+import constantes from "../constants/equipoConstants";
+
+export default {
+    saveEquipo: (nombre) => {
+        dispatcher.dispatch({
+            type: constantes.SAVE_EQUIPO,
+            nombre: nombre
+        });
+    }
+}
